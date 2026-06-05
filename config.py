9023d2b47bfc14fa0954a738b248f8e53e38a62b
Shennice65 <shennice65@gmail.com>
2026-06-05 20:54:27 +0800

    Fixed environment variable loading

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Config:
-    TOKEN = os.getenv('DISCORD_TOKEN')
-    GUILD_ID = int(os.getenv('GUILD_ID', 0))
-    OBSERVER_ROLE_ID = int(os.getenv('OBSERVER_ROLE_ID', 0))
-    LOG_CHANNEL_ID = int(os.getenv('LOG_CHANNEL_ID', 0))
-    TICKET_CATEGORY_ID = int(os.getenv('TICKET_CATEGORY_ID', 0))
+    TOKEN = os.environ.get('DISCORD_TOKEN') or os.getenv('DISCORD_TOKEN')
+    GUILD_ID = int(os.environ.get('GUILD_ID', 0))
+    OBSERVER_ROLE_ID = int(os.environ.get('OBSERVER_ROLE_ID', 0))
+    LOG_CHANNEL_ID = int(os.environ.get('LOG_CHANNEL_ID', 0))
+    TICKET_CATEGORY_ID = int(os.environ.get('TICKET_CATEGORY_ID', 0))
     DATA_FILE = 'bot_data.json'
